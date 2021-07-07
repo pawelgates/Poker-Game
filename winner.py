17acdbcba37675f95d95732e0d00c7c1a1c131ea
Pavel <pawel.gates@gmail.com>
2021-07-07 19:01:40 +0300

    init

--- winner.py
+++ winner.py
@@ -4,3 +4,6 @@ from models import Player, Dealer, Table
 def cards_combination(player, table):
     combination = player.player_cards() + table.table_cards()
     return combination
+
+
+print("hi")
